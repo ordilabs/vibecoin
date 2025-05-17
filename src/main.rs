6f0358880a593a36c59bbd4d3606d771099afd91
Felix Weis <551068+FelixWeis@users.noreply.github.com>
2025-05-17 18:57:11 +0200

    Add money utilities in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bitcoin::blockdata::constants::genesis_block;
 use bitcoin::network::constants::Network;
 use bitcoin::consensus::encode::serialize_hex;
+mod utils;
 
 fn genesis_hex() -> String {
     let genesis = genesis_block(Network::Bitcoin);
